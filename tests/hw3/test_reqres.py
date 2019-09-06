aa8cbe2e70ff98d8178fd368374aab62036678c4
Valeria_Grace <v.smirnova@group-ib.ru>
2019-09-06 13:56:53 +0300

    change basic_url() method to a base_url var

--- tests/hw3/test_reqres.py
+++ tests/hw3/test_reqres.py
@@ -3,23 +3,19 @@ import pytest
 import requests
 import jsonpath
 
-
-def basic_url():
-    """defines the basic url we send"""
-    url = "https://reqres.in"
-    return url
+base_url = "https://reqres.in"
 
 
 def test_get_response():
     """this is a basic test for getting a response and asserting its 200"""
-    response = requests.get('https://reqres.in/api/users?page=2')
+    response = requests.get( base_url + '/api/users?page=2')
     print(response)
     assert response.status_code == 200
 
 
 def test_check_type_of_content():
     """checking the type of an id"""
-    response = requests.get(basic_url() + '/api/users?page=2')
+    response = requests.get(base_url + '/api/users?page=2')
     json_response = json.loads(response.text)
     print(json_response)
     total_pages = jsonpath.jsonpath(json_response, 'total_pages')
@@ -32,7 +28,7 @@ def test_create_new_user_using_json_file():
     with open("new_user.json") as file:
         json_input = file.read()
     request_json = json.loads(json_input)
-    response = requests.post(basic_url() + '/api/users', request_json)
+    response = requests.post(base_url + '/api/users', request_json)
     print(response.content)
     assert response.status_code == 201
     response_json = json.loads(response.text)  # here we parse the response to a json format
@@ -49,7 +45,7 @@ DATA = [("michael.lawson@reqres.in", "1234"),
 @pytest.mark.parametrize("email, password", DATA)
 def test_register_new_user_parametrized(email, password):
     """try to register a few users using pytest.mark.parametrize"""
-    response = requests.post(basic_url() + '/api/register',
+    response = requests.post(base_url + '/api/register',
                              data={"email": email, "password": password})
     print(response.content)
     assert response.status_code == 200
@@ -59,6 +55,6 @@ def test_register_new_user_parametrized(email, password):
 @pytest.mark.parametrize("job", ["job1, job2, job3"])
 def test_reg_params(name, job):
     """register a new user using mark.parametrize"""
-    response = requests.post(basic_url() + '/api/users', data={"name": name, "job": job})
+    response = requests.post(base_url + '/api/users', data={"name": name, "job": job})
     print(response.content)
     assert response.status_code == 201
