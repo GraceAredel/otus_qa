824617e4f73e98da41090056a305f1854a304d30
Valeria_Grace <v.smirnova@group-ib.ru>
2019-09-06 19:27:05 +0300

    changed task 4 in hw

--- tests/hw3/test_answer.py
+++ tests/hw3/test_answer.py
@@ -3,3 +3,5 @@ import requests
 
 def test_answer(url_param):
     """test to check the status code from url through addoption"""
+    response = requests.get(url_param)
+    assert response.status_code == 200
