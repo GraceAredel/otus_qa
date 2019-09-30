d5cac23f1420e8e11f86fabdedcd2738d5c64183
Grace Aredel <kataramoonlight@gmail.com>
2019-09-30 12:33:33 +0300

    another fixes

--- tests/hw3/test_answer.py
+++
@@ -1,8 +0,0 @@
-"""basic test to check status code from given url"""
-import requests
-
-
-def test_answer(url_param):
-    """test to check the status code from url through addoption"""
-    response = requests.get(url_param)
-    assert response.status_code == 200
